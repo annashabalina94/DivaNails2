82986d67ea8c57b8ef39488432fa58afbd690c48
Anna Shabalina <annashabalina@Annas-Mac.local>
2017-10-31 00:54:26 -0700

    Initial Commit

---
+++ DivaNails2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DivaNails2
+//
+//  Created by Anna Shabalina on 10/31/17.
+//  Copyright © 2017 Anna Shabalina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
