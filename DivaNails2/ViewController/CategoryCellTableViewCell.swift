39e4ad9a888c6dbeeb9a8cc5c6d76af19a459c4b
Anna Shabalina <annashabalina@Annas-Mac.local>
2017-11-01 17:19:05 -0700

    Here is my commit message

---
+++ DivaNails2/ViewController/CategoryCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCellTableViewCell.swift
+//  DivaNails2
+//
+//  Created by Anna Shabalina on 10/31/17.
+//  Copyright © 2017 Anna Shabalina. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DivaNails2/ViewController/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  DivaNails2
+//
+//  Created by Anna Shabalina on 10/31/17.
+//  Copyright © 2017 Anna Shabalina. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
